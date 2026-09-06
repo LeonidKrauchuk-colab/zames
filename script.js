b22c4dff7d87c8d1eb5b1dfca6155b9cfd9d0247
LeonidKrauchuk-colab <leon12439@gmail.com>
2026-09-07 00:25:32 +0300

    favicon

--- script.js
+++ script.js
@@ -1,45 +1,42 @@
 
-let benzVаlue=document.getElementById('benzin');
-let outBenz=document.querySelector('.outBenz');
-let outOil=document.querySelector('.outOil');
-let oil=40;
+let benzVаlue = document.getElementById('benzin');
+let outBenz = document.querySelector('.outBenz');
+let outOil = document.querySelector('.outOil');
+let oil = 40;
 const selectedRadioEzda = document.querySelectorAll('input[name="ezda"]');
 const selectedRadioTip = document.querySelectorAll('input[name="variantOil"]');
-const selectedRadio=document.querySelectorAll('input[type="radio"]');
+const selectedRadio = document.querySelectorAll('input[type="radio"]');
 
-outOil.innerHTML=zames(benzVаlue.value,oil);
+outOil.innerHTML = zames(benzVаlue.value, oil);
 
-function zames(a,b){
+function zames(a, b) {
     if (selectedRadioTip[0].checked & selectedRadioEzda[0].checked) {
-        oil=25;
-        b=oil
+        oil = 25;
+        b = oil
     }
     if (selectedRadioTip[0].checked & selectedRadioEzda[1].checked) {
-        oil=20;
-        b=oil
+        oil = 20;
+        b = oil
     }
     if (selectedRadioTip[1].checked & selectedRadioEzda[1].checked) {
-        oil=25;
-        b=oil
+        oil = 25;
+        b = oil
     }
     if (selectedRadioTip[1].checked & selectedRadioEzda[0].checked) {
-        oil=33;
-        b=oil
+        oil = 33;
+        b = oil
     }
-return Math.round((a/b)*1000);
-} 
+    return Math.round((a / b) * 1000);
+}
 
 for (let index = 0; index < selectedRadio.length; index++) {
-    selectedRadio[index].onclick=function () {
-    outBenz.innerHTML = benzVаlue.value;
-    outOil.innerHTML=zames(benzVаlue.value,oil);   
-    }   
+    selectedRadio[index].onclick = function () {
+        outBenz.innerHTML = benzVаlue.value;
+        outOil.innerHTML = zames(benzVаlue.value, oil);
+    }
 }
 
-benzVаlue.oninput = function(){
+benzVаlue.oninput = function () {
     outBenz.innerHTML = benzVаlue.value;
-    outOil.innerHTML=zames(benzVаlue.value,oil);
-
+    outOil.innerHTML = zames(benzVаlue.value, oil);
 }
-
-
