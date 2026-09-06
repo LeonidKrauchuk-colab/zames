008c5845fe37aa34e3a2e436352ef6d18760da8a
LeonidKrauchuk-colab <leon12439@gmail.com>
2026-09-06 20:45:56 +0300

    второе сохранение

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
 
-let benzVаlue=document.getElementById('benz');
-console.log(benzVаlue.value);
+let benzVаlue=document.getElementById('benzin');
 let outBenz=document.querySelector('.outBenz');
 let outOil=document.querySelector('.outOil');
-console.log(outOil);
-const oilProstoeEzda=40;
+let oil=40;
+const selectedRadioEzda = document.querySelectorAll('input[name="ezda"]');
+const selectedRadioTip = document.querySelectorAll('input[name="variantOil"]');
+const selectedRadio=document.querySelectorAll('input[type="radio"]');
 
+outOil.innerHTML=zames(benzVаlue.value,oil);
 
-benzVаlue.oninput = function() {
+function zames(a,b){
+    if (selectedRadioTip[0].checked & selectedRadioEzda[0].checked) {
+        oil=25;
+        b=oil
+    }
+    if (selectedRadioTip[0].checked & selectedRadioEzda[1].checked) {
+        oil=20;
+        b=oil
+    }
+    if (selectedRadioTip[1].checked & selectedRadioEzda[1].checked) {
+        oil=25;
+        b=oil
+    }
+    if (selectedRadioTip[1].checked & selectedRadioEzda[0].checked) {
+        oil=33;
+        b=oil
+    }
+return Math.round((a/b)*1000);
+} 
 
+for (let index = 0; index < selectedRadio.length; index++) {
+    selectedRadio[index].onclick=function () {
     outBenz.innerHTML = benzVаlue.value;
-    
-outOil.innerHTML=+benzVаlue.value*oilProstoeEzda;
+    outOil.innerHTML=zames(benzVаlue.value,oil);   
+    }   
+}
 
-  };
+benzVаlue.oninput = function(){
+    outBenz.innerHTML = benzVаlue.value;
+    outOil.innerHTML=zames(benzVаlue.value,oil);
+
+}
+
+
